4f5a9ea4321d4cfe62aefa7960c37b366a750958
jaco9419 <jaco.9419@gmail.com>
2020-07-20 11:35:24 -0300

    Updates

---
+++ precache-manifest.4885d2b72d464f60ac8f3f46356b3851.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "183c0b91831bf5264b677576656f3daf",
+    "url": "/Youtube-Clone/index.html"
+  },
+  {
+    "revision": "413ea805a6409b3451b2",
+    "url": "/Youtube-Clone/static/js/2.ff8656a5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Youtube-Clone/static/js/2.ff8656a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b97b89950a872548e4e",
+    "url": "/Youtube-Clone/static/js/main.ae258715.chunk.js"
+  },
+  {
+    "revision": "2c3b149e6ee59968c7c2",
+    "url": "/Youtube-Clone/static/js/runtime-main.16593b08.js"
+  }
+]);
