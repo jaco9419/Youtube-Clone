dc1e24da3b9379634385f3a05931aa986a497849
jaco9419 <jaco.9419@gmail.com>
2020-09-17 12:32:49 -0300

    Add node_modules

---
+++ build/precache-manifest.203a7cc9545891064b242df6356ae93c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ee8c497780588e24b66dda2e528d80",
+    "url": "/Youtube-Clone/index.html"
+  },
+  {
+    "revision": "e60966b9baf97b786af4",
+    "url": "/Youtube-Clone/static/css/main.36da617a.chunk.css"
+  },
+  {
+    "revision": "9cbdf722c9a3ec1afe45",
+    "url": "/Youtube-Clone/static/js/2.874248e5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Youtube-Clone/static/js/2.874248e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e60966b9baf97b786af4",
+    "url": "/Youtube-Clone/static/js/main.7f48943f.chunk.js"
+  },
+  {
+    "revision": "2c3b149e6ee59968c7c2",
+    "url": "/Youtube-Clone/static/js/runtime-main.16593b08.js"
+  },
+  {
+    "revision": "4be78f8e0f5e20d6871ef99b636402eb",
+    "url": "/Youtube-Clone/static/media/youtube-logo.4be78f8e.svg"
+  }
+]);
